666f236c630a274123138299f6fdf1aa28635739
Xueliang (Albert) Han <xmail.han@gmail.com>
2013-03-14 12:55:07 +0800

    Update

--- markdown-generate.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash env
-
-DIR="~/publishing/xuelianghan.github.com/tools"
-cd $DIR
-exec perl `~/code/perl/markdown.pl` "$DIR/tools-of-craftsman.markdown" > "$DIR/tools-of-craftsman.html"
-
-
