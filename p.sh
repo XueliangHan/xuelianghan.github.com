93ea4afe59fa0385b54ce60f1d254f8bfe73ecef
Xueliang (Albert) Han <xmail.han@gmail.com>
2013-03-23 11:51:55 +0800

    Update

--- p.sh
+++ p.sh
@@ -2,7 +2,7 @@
 
 perl md.pl resume.markdown > resume.html
 perl md.pl project.markdown > project.html
-perl md.pl tools/tools-craftsman.markdown > tools-craftsman.html
+perl md.pl tools/tools-of-craftsman.markdown > tools/tools-of-craftsman.html
 
 git add .
 git commit -a -m "Update"
