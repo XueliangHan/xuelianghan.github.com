83daa716a2075c3be4890a17f88b941b43dd2740
Xueliang (Albert) Han <xmail.han@gmail.com>
2013-04-07 00:46:00 +0800

    Update

--- generate.sh
+++ generate.sh
@@ -12,14 +12,14 @@ perl md.pl resume.markdown > resume.html
 perl md.pl jianli.markdown > jianli.html
 perl md.pl skill.markdown > skill.html
 perl md.pl tool.markdown > tool.html
-perl md.pl misc.markdown > misc.html
+perl md.pl misc.markdown > misc.html #
 perl md.pl personal-development.markdown > personal-development.html
 
 
 
 #DIR draft
-perl md.pl ./cs/linux.markdown > ./cs/linux.html
-perl md.pl ./cs/LISP.markdown > ./cs/LISP.html
+perl md.pl ./cs/linux.markdown > ./cs/linux.html #
+perl md.pl ./cs/LISP.markdown > ./cs/LISP.html #
 
 perl md.pl ./masters/Alan_Kay.markdown > ./masters/Alan_Kay.html
 perl md.pl ./masters/Steve_Jobs.markdown > ./masters/Steve_Jobs.html
