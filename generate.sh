8e5930419648728379cfe91b432d855a49e8829f
Xueliang (Albert) Han @ Windows <xmail.han@gmail.com>
2013-04-17 21:28:06 +0800

    refactor generate.sh:

--- generate.sh
+++ generate.sh
@@ -1,30 +1,34 @@
 #!/bin/sh
 
-D="/home/h/publishing/xuelianghan.github.com"
+
+#Design, SRC is where I put my markdown file, DES is where I want to generate the html from markdown.
+SRC="/home/h/publishing/xuelianghan.github.com"
+DES=""
+
 
 #Site structure
-perl md.pl $D/hobby.markdown > hobby.html
+perl md.pl $SRC/hobby.markdown > hobby.html
 
-perl md.pl $D/interest.markdown > interest.html
-perl md.pl $D/interest_technology.markdown > interest_technology.html
+perl md.pl $SRC/interest.markdown > interest.html
+perl md.pl $SRC/interest_technology.markdown > interest_technology.html
 
-perl md.pl $D/information-source.markdown > information-source.html
-perl md.pl $D/project.markdown > project.html
-perl md.pl $D/resume.markdown > resume.html
-perl md.pl $D/jianli.markdown > jianli.html
-perl md.pl $D/skill.markdown > skill.html
-perl md.pl $D/tool.markdown > tool.html
-perl md.pl $D/misc.markdown > misc.html #
+perl md.pl $SRC/information-source.markdown > information-source.html
+perl md.pl $SRC/project.markdown > project.html
+perl md.pl $SRC/resume.markdown > resume.html
+perl md.pl $SRC/jianli.markdown > jianli.html
+perl md.pl $SRC/skill.markdown > skill.html
+perl md.pl $SRC/tool.markdown > tool.html
+perl md.pl $SRC/misc.markdown > misc.html #
 
-perl md.pl $D/personal-development.markdown > personal-development.html
+perl md.pl $SRC/personal-development.markdown > personal-development.html
 
 
 #DIR draft
-perl md.pl $D/cs/linux.markdown > $D/cs/linux.html #
-perl md.pl $D/cs/LISP.markdown > $D/cs/LISP.html #
-perl md.pl $D/cs/algorithms_data-structure.markdown > $D/cs/algorithms_data-structure.html
+perl md.pl $SRC/cs/linux.markdown > $SRC/cs/linux.html #
+perl md.pl $SRC/cs/LISP.markdown > $SRC/cs/LISP.html #
+perl md.pl $SRC/cs/algorithms_data-structure.markdown > $SRC/cs/algorithms_data-structure.html
 
-perl md.pl $D/masters/Alan_Kay.markdown > $D/masters/Alan_Kay.html
-perl md.pl $D/masters/Steve_Jobs.markdown > $D/masters/Steve_Jobs.html
+perl md.pl $SRC/masters/Alan_Kay.markdown > $SRC/masters/Alan_Kay.html
+perl md.pl $SRC/masters/Steve_Jobs.markdown > $SRC/masters/Steve_Jobs.html
 
-perl md.pl $D/draft/what-is-programming.markdown > $D/draft/what-is-programming.html
+perl md.pl $SRC/draft/what-is-programming.markdown > $SRC/draft/what-is-programming.html
