7fccb7ccbc1f2afe41b25b66b19b812e1c0eb879
Xueliang Han @ Windows.7 <xmail.han@gmail.com>
2013-06-26 15:56:27 +0800

    Content update

--- generate.sh
+++ generate.sh
@@ -34,12 +34,15 @@ fi
 
 #Previous bash script in convert.sh
 #for f in `find . -name "*.markdown"` ;
-#    do perl markdown.pl $f > $f.html;
+#    do perl markdown.pl $f > $f.html";
     #use sed to trunc the file extension
     #construct the site map with prefix(fixed url link)
 #done
 
 ## sitemap.sh  
+
+
+
 
 
 #####################################################################################
@@ -49,101 +52,97 @@ fi
 #html manually wrote
 #  index.html
 #  about.html
-perl md.pl $SRC/links.markdown    > $DES/links.html
+perl md.pl "$SRC/links.markdown"    > "$DES/links.html"
 #  status-update.html
 #  statistics.html
 #  programming language
+
+
+
 
 
 #####################################################################################
 #                                       LIFE                                        #
 #####################################################################################
 #0 life
-perl md.pl $SRC/un/information-source.markdown   > $DES/un/information-source.html
-perl md.pl $SRC/un/inspiration.markdown          > $DES/un/inspiration.html
-perl md.pl $SRC/un/interest.markdown             > $DES/un/interest.html
-perl md.pl $SRC/un/interest_technology.markdown  > $DES/un/interest_technology.html
-
+perl md.pl "$SRC/un/information-source.markdown"        > "$DES/un/information-source.html"
+perl md.pl "$SRC/un/inspiration.markdown"               > "$DES/un/inspiration.html"
+perl md.pl "$SRC/un/interest.markdown"                  > "$DES/un/interest.html"
+perl md.pl "$SRC/un/interest_technology.markdown"       > "$DES/un/interest_technology.html"
 
 ##Essays
-perl md.pl $SRC/essays/essays.markdown        > $DES/essays/essays.html
+perl md.pl "$SRC/essays/essays.markdown"                > "$DES/essays/essays.html"
 
 ##Hobbies
-perl md.pl $SRC/hobbies/hobbies.markdown            > $DES/hobbies/hobbies.html
-#perl md.pl $SRC/hobbies/film-and-movie.markdown     > $DES/hobbies/film-and-movie.html
-perl md.pl $SRC/hobbies/meditation.markdown         > $DES/hobbies/meditation.html
-perl md.pl $SRC/hobbies/music.markdown              > $DES/hobbies/music.html
-#perl md.pl $SRC/hobbies/reading.markdown            > $DES/hobbies/reading.html
-#perl md.pl $SRC/hobbies/sports-and-fitness.markdown > $DES/hobbies/sports-and-fitness.html
-#perl md.pl $SRC/hobbies/travel.markdown             > $DES/hobbies/travel.html
-#perl md.pl $SRC/hobbies/.markdown   > $DES/hobbies/.html
-
+perl md.pl "$SRC/hobbies/hobbies.markdown"              > "$DES/hobbies/hobbies.html"
+#perl md.pl "$SRC/hobbies/film-and-movie.markdown"      > "$DES/hobbies/film-and-movie.html"
+perl md.pl "$SRC/hobbies/meditation.markdown"           > "$DES/hobbies/meditation.html"
+perl md.pl "$SRC/hobbies/music.markdown"                > "$DES/hobbies/music.html"
+perl md.pl "$SRC/hobbies/sci-fi.markdown"               > "$DES/hobbies/sci-fi.html"
+#perl md.pl "$SRC/hobbies/reading.markdown"             > "$DES/hobbies/reading.html"
+#perl md.pl "$SRC/hobbies/sports-and-fitness.markdown"  > "$DES/hobbies/sports-and-fitness.html"
+#perl md.pl "$SRC/hobbies/travel.markdown"              > "$DES/hobbies/travel.html"
 
 ##people and masters
-perl md.pl $SRC/people/links.markdown          > $DES/people/links.html
-perl md.pl $SRC/people/masters.markdown        > $DES/people/masters.html
-perl md.pl $SRC/people/person.markdown         > $DES/people/person.html
-perl md.pl $SRC/people/Alan_Kay.markdown       > $DES/people/Alan_Kay.html
-perl md.pl $SRC/people/Jianzhong_Lee.markdown  > $DES/people/Jianzhong_Lee.html
-perl md.pl $SRC/people/Linus_Torvalds.markdown > $DES/people/Linus_Torvalds.html
-perl md.pl $SRC/people/Steve_Jobs.markdown     > $DES/people/Steve_Jobs.html
-
+perl md.pl "$SRC/people/links.markdown"                 > "$DES/people/links.html"
+perl md.pl "$SRC/people/masters.markdown"               > "$DES/people/masters.html"
+perl md.pl "$SRC/people/person.markdown"                > "$DES/people/person.html"
+perl md.pl "$SRC/people/Alan_Kay.markdown"              > "$DES/people/Alan_Kay.html"
+perl md.pl "$SRC/people/Jianzhong_Lee.markdown"         > "$DES/people/Jianzhong_Lee.html"
+perl md.pl "$SRC/people/Linus_Torvalds.markdown"        > "$DES/people/Linus_Torvalds.html"
+perl md.pl "$SRC/people/Steve_Jobs.markdown"            > "$DES/people/Steve_Jobs.html"
 
 ##Quotes
-perl md.pl $SRC/quotes/quotes.markdown                 > $DES/quotes/quotes.html
-perl md.pl $SRC/quotes/albert-einstein-quotes.markdown > $DES/quotes/albert-einstein-quotes.html
-
+perl md.pl "$SRC/quotes/quotes.markdown"                > "$DES/quotes/quotes.html"
+perl md.pl "$SRC/quotes/albert-einstein-quotes.markdown"> "$DES/quotes/albert-einstein-quotes.html"
 
 ##References
-perl md.pl $SRC/references/references.markdown > $DES/references/references.html
-perl md.pl $SRC/references/taxonomy.markdown   > $DES/references/taxonomy.html
-perl md.pl $SRC/references/symbols.markdown    > $DES/references/symbols.html
+perl md.pl "$SRC/references/references.markdown"        > "$DES/references/references.html"
+perl md.pl "$SRC/references/taxonomy.markdown"          > "$DES/references/taxonomy.html"
+perl md.pl "$SRC/references/symbols.markdown"           > "$DES/references/symbols.html"
+
+
 
 
 
 #####################################################################################
 #                                       WORKS                                       #
 #####################################################################################
-
 ##1 Professional
-perl md.pl $SRC/un/projects.markdown > $DES/un/projects.html
-perl md.pl $SRC/files/resume/resume.markdown   > $DES/files/resume/resume.html
-perl md.pl $SRC/files/resume/jianli.markdown   > $DES/files/resume/jianli.html
-perl md.pl $SRC/skillsets/skillsets.markdown   > $DES/skillsets/skillsets.html
-
-#perl md.pl $SRC/programming-languages.markdown > $DES/programming-languages.html
-
-perl md.pl $SRC/cs/algorithms_data-structure.markdown > $DES/cs/algorithms_data-structure.html
-perl md.pl $SRC/cs/linux.markdown                     > $DES/cs/linux.html
-perl md.pl $SRC/cs/linux_command.markdown             > $DES/cs/linux_command.html
-perl md.pl $SRC/cs/linux-distro-arch-linux.markdown   > $DES/cs/linux-distro-arch-linux.html
-perl md.pl $SRC/cs/linux-distro-goobuntu.markdown     > $DES/cs/linux-distro-goobuntu.html
-perl md.pl $SRC/cs/networking.markdown                > $DES/cs/networking.html
-perl md.pl $SRC/cs/networking_command.markdown        > $DES/cs/networking_command.html
-
+perl md.pl "$SRC/un/projects.markdown"                  > "$DES/un/projects.html"
+perl md.pl "$SRC/files/resume/resume.markdown"          > "$DES/files/resume/resume.html"
+perl md.pl "$SRC/files/resume/jianli.markdown"          > "$DES/files/resume/jianli.html"
+perl md.pl "$SRC/skillsets/skillsets.markdown"          > "$DES/skillsets/skillsets.html"
+#perl md.pl "$SRC/programming-languages.markdown" > "$DES/programming-languages.html"
+perl md.pl "$SRC/cs/cognition-of-cs.markdown"           > "$DES/cs/cognition-of-cs.html"
+perl md.pl "$SRC/cs/algorithms_data-structure.markdown" > "$DES/cs/algorithms_data-structure.html"
+perl md.pl "$SRC/cs/linux.markdown"                     > "$DES/cs/linux.html"
+perl md.pl "$SRC/cs/linux_command.markdown"             > "$DES/cs/linux_command.html"
+perl md.pl "$SRC/cs/linux-distro-arch-linux.markdown"   > "$DES/cs/linux-distro-arch-linux.html"
+perl md.pl "$SRC/cs/linux-distro-goobuntu.markdown"     > "$DES/cs/linux-distro-goobuntu.html"
+perl md.pl "$SRC/cs/networking.markdown"                > "$DES/cs/networking.html"
+perl md.pl "$SRC/cs/networking_command.markdown"        > "$DES/cs/networking_command.html"
 
 #pl
-perl md.pl $SRC/cs/pl/programming-language.markdown > $DES/cs/pl/programming-language.html
-perl md.pl $SRC/cs/pl/c.markdown                     > $DES/cs/pl/c.html
-perl md.pl $SRC/cs/pl/LISP.markdown                  > $DES/cs/pl/LISP.html #
-perl md.pl $SRC/cs/pl/python.markdown                > $DES/cs/pl/python.html
-perl md.pl $SRC/cs/pl/bash.markdown                  > $DES/cs/pl/bash.html
-perl md.pl $SRC/cs/pl/HTML.markdown                  > $DES/cs/pl/HTML.html
-perl md.pl $SRC/cs/pl/markdown.markdown              > $DES/cs/pl/markdown.html
+perl md.pl "$SRC/cs/pl/programming-language.markdown"   > "$DES/cs/pl/programming-language.html"
+perl md.pl "$SRC/cs/pl/c.markdown"                      > "$DES/cs/pl/c.html"
+perl md.pl "$SRC/cs/pl/LISP.markdown"                   > "$DES/cs/pl/LISP.html" #
+perl md.pl "$SRC/cs/pl/python.markdown"                 > "$DES/cs/pl/python.html"
+perl md.pl "$SRC/cs/pl/bash.markdown"                   > "$DES/cs/pl/bash.html"
+perl md.pl "$SRC/cs/pl/HTML.markdown"                   > "$DES/cs/pl/HTML.html"
+perl md.pl "$SRC/cs/pl/markdown.markdown"               > "$DES/cs/pl/markdown.html"
 
 ##draft
-perl md.pl $SRC/drafts/what-is-programming.markdown > $DES/drafts/what-is-programming.html
+perl md.pl "$SRC/drafts/what-is-programming.markdown"   > "$DES/drafts/what-is-programming.html"
 
 ##hacks
-perl md.pl $SRC/hacks/hacks.markdown                                                          > $DES/hacks/hacks.html
-perl md.pl $SRC/hacks/reverse-engineering-how-to-convert-epub-book-into-html-doc-pdf.markdown > $DES/hacks/reverse-engineering-how-to-convert-epub-book-into-html-doc-pdf.html
-perl md.pl $SRC/hacks/use-goagent-and-gae-to-setup-a-free-and-open-internet-access.markdown   > $DES/hacks/use-goagent-and-gae-to-setup-a-free-and-open-internet-access.html
-
-
+perl md.pl "$SRC/hacks/hacks.markdown"                                                          > "$DES/hacks/hacks.html"
+perl md.pl "$SRC/hacks/reverse-engineering-how-to-convert-epub-book-into-html-doc-pdf.markdown" > "$DES/hacks/reverse-engineering-how-to-convert-epub-book-into-html-doc-pdf.html"
+perl md.pl "$SRC/hacks/use-goagent-and-gae-to-setup-a-free-and-open-internet-access.markdown"   > "$DES/hacks/use-goagent-and-gae-to-setup-a-free-and-open-internet-access.html"
 
 ##toolkits
-perl md.pl $SRC/toolkits/toolkits.markdown    > $DES/toolkits/toolkits.html
-perl md.pl $SRC/toolkits/gcc.markdown         > $DES/toolkits/gcc.html
-perl md.pl $SRC/toolkits/git.markdown         > $DES/toolkits/git.html
-perl md.pl $SRC/toolkits/text-editor.markdown > $DES/toolkits/text-editor.html
-perl md.pl $SRC/toolkits/vi-vim.markdown      > $DES/toolkits/vi-vim.html
+perl md.pl "$SRC/toolkits/toolkits.markdown"            > "$DES/toolkits/toolkits.html"
+perl md.pl "$SRC/toolkits/gcc.markdown"                 > "$DES/toolkits/gcc.html"
+perl md.pl "$SRC/toolkits/git.markdown"                 > "$DES/toolkits/git.html"
+perl md.pl "$SRC/toolkits/text-editor.markdown"         > "$DES/toolkits/text-editor.html"
+perl md.pl "$SRC/toolkits/vi-vim.markdown"              > "$DES/toolkits/vi-vim.html"
--- template.markdown
+++ template.markdown
@@ -1,3 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><?xml version="1.0" encoding="utf-8"?><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><head><meta charset='utf-8'><meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+<title>    </title><link id="MainCSS" type="text/css" rel="stylesheet" href="../static/stylesheets/main.css"/><script type="text/javascript" src=""></script></head><body>
+<div align="center"><h1>    </h1><div>
+
+<div id="quote">
+  <div>A pursuit outside one's regular occupation engages in especially for <strong>relaxation</strong>.</div>
+  <div align="right">Definition of hobby from Marriam-Webster dictionary.</div>
+</div>
+
+<!-- compressed format-->
+
+</body></html>
+
+
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><?xml version="1.0" encoding="utf-8"?><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
   <meta charset='utf-8'>
@@ -7,12 +22,11 @@
 </head><body>
 <div align="center"><h1></h1><div>
 
-</body></html>
+<div id="quote">
+  <div>A pursuit outside one's regular occupation engages in especially for <strong>relaxation</strong>.</div>
+  <div align="right">Definition of hobby from Marriam-Webster dictionary.</div>
+</div>
 
-
-<!-- compressed format-->
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><?xml version="1.0" encoding="utf-8"?><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><head><meta charset='utf-8'><meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-<title>    </title><link id="MainCSS" type="text/css" rel="stylesheet" href="../static/stylesheets/main.css"/><script type="text/javascript" src=""></script></head><body>
-<div align="center"><h1>    </h1><div>
+<!-- formated -->
 
 </body></html>
